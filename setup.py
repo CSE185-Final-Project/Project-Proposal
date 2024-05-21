27a94fac5d1b5b812b0f8bdb22992d29c8bf3bc4
JerryLiu314 <jil314@ucsd.edu>
2024-05-21 15:59:59 -0700

    bug fixing

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'run_pipeline=scripts.run_pipeline:main',
+            'scRNAseq_Easy=scripts.scRNAseq_Easy:main',
         ],
     },
 )
