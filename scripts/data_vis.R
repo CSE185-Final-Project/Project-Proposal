ee333965c9c37ebf4a93e7d778853a47bf2cf91c
JerryLiu314 <jil314@ucsd.edu>
2024-05-28 00:49:35 -0700

    R test

--- scripts/data_vis.R
+++ scripts/data_vis.R
@@ -1,57 +1,93 @@
+cat("Start to install necessary packages\n")
+silent_install <- function(pkg) {
+  if (!require(pkg, character.only = TRUE)) {
+    suppressMessages(suppressWarnings(install.packages(pkg, quiet = TRUE)))
+    suppressMessages(suppressWarnings(library(pkg, character.only = TRUE)))
+  }
+}
+silent_install("jsonlite")
+silent_install("DESeq2")
+silent_install("tximport")
+silent_install("ggplot2")
 library("jsonlite")
 library("DESeq2")
 library("tximport")
+library("ggplot2")
+
+cat("Finish installing\n")
 
 args <- commandArgs(trailingOnly = TRUE)
+
 data1 <- args[1]
 data2 <- args[2]
+
 output_path <- args[3]
+output_path <- trimws(output_path)
+
+p_value <- as.numeric(args[4])
+
+fod <- as.numeric(args[5])
+fod <- log2(fod)
+
+filter <- as.numeric(args[6])
+
+name <- args[7]
+name <- trimws(name)
 
 data_1_v <- fromJSON(data1)
 data_2_v <- fromJSON(data2)
 files <- c(data_1_v, data_2_v)
-num_samples <- length(data_1_v)
+num_samples_1 <- length(data_1_v)
+num_samples_2 <- length(data_1_v)
+smallestGroupSize <- min(c(num_samples_1, num_samples_2))
 
-for (file in data_1_v) {
-  print(file)
+group_1 <- c()
+group_2 <- c()
+conditions <- c(rep("Group1", num_samples_1), rep("Group2", num_samples_2))
+for (i in 1:num_samples_1) {
+  group_1 <- c(group_1, paste("Group1_sample", i))
 }
-# conditions <- c(rep("Group1", num_samples), rep("Group2", num_samples))
-# for (i in 1:num_samples) {
-#   group_1 <- c(group_1, paste("Group1_sample", i))
-# }
-# for (i in 1:num_samples) {
-#   group_2 <- c(group_2, paste("Group2_sample", i))
-# }
+for (i in 1:num_samples_2) {
+  group_2 <- c(group_2, paste("Group2_sample", i))
+}
 
-# samples <- data.frame("run" = c(group_1, group_2), "condition"  = conditions)
-# names(files) <- samples$run
+samples <- data.frame("run" = c(group_1, group_2), "condition"  = conditions)
+names(files) <- samples$run
 
-# txi <- tximport(files, type="rsem")
-# txi$length[txi$length == 0] <- 1
-# ddsTxi <- DESeqDataSetFromTximport(txi,
-#                                    colData = samples,
-#                                    design = ~ condition)
+txi <- tximport(files, type="rsem")
+txi$length[txi$length == 0] <- 1
+ddsTxi <- DESeqDataSetFromTximport(txi,
+                                   colData = samples,
+                                   design = ~ condition)
 
-# # TODO
-# smallestGroupSize <- 3
-# keep <- rowSums(counts(ddsTxi) >= 10) >= smallestGroupSize
-# ddsTxi <- ddsTxi[keep,]
-# ddsTxi <- DESeq(ddsTxi)
-# df <- results(ddsTxi)
-# df$diffexpressed <- "NO"
-# df$diffexpressed[df$log2FoldChange > 0 & df$padj < 0.05] <- "UP"
-# df$diffexpressed[df$log2FoldChange < 0 & df$padj < 0.05] <- "DOWN"
-# df$delabel <- ifelse(df$X %in% head(df[order(df$padj), "X"], 30), df$X, NA)
-
-# p <-ggplot(data = df, aes(x = log2FoldChange, y = -log10(padj), col = diffexpressed, label = delabel)) +
-#   geom_vline(xintercept = 0, col = "gray", linetype = 'dashed') +
-#   geom_hline(yintercept = -log10(0.05), col = "gray", linetype = 'dashed') + 
-#   geom_point(size = 1) + 
-#   scale_color_manual(values = c("#00AFBB", "grey", "#bb0c00"), # to set the colours of our variable  
-#                      labels = c("Downregulated", "Not significant", "Upregulated")) + # to set the labels in case we want to overwrite the categories from the dataframe (UP, DOWN, NO)
-#   coord_cartesian(ylim = c(0, 50), xlim = c(-10, 10)) + # since some genes can have minuslog10padj of inf, we set these limits
-#   labs(color = 'Severe', #legend_title, 
-#        x = expression("log"[2]*"FoldChange"), y = expression("-log"[10]*"p-value")) + 
-#   scale_x_continuous(breaks = seq(-10, 10, 2)) + # to customise the breaks in the x axis
-#   ggtitle('volcano plot--ddsTxi result') + 
-#   geom_text() 
+# TODO
+keep <- rowSums(counts(ddsTxi) >= filter) >= smallestGroupSize
+ddsTxi <- ddsTxi[keep,]
+ddsTxi <- DESeq(ddsTxi)
+df <- results(ddsTxi)
+df <- as.data.frame(df)
+df$diffexpressed <- "NO"
+df$diffexpressed[df$log2FoldChange > fod & df$padj < p_value] <- "UP"
+df$diffexpressed[df$log2FoldChange < fod & df$padj < p_value] <- "DOWN"
+df$X <- row.names(df)
+if (name == "NO") {
+  df$delabel <- ifelse(df$X %in% head(df[order(df$padj), "X"], 30), df$X, NA)
+} else {
+  df_name <- read.csv(name, header = FALSE, sep = "\t")
+  colnames(df_name) <- c("X", "Name")
+  df <- merge(df, df_name, by = "X", all.x = TRUE)
+  df$delabel <- ifelse(df$Name %in% head(df[order(df$padj), "Name"], 30), df$Name, NA)
+}
+p <-ggplot(data = df, aes(x = log2FoldChange, y = -log10(padj), col = diffexpressed, label = delabel)) +
+  geom_vline(xintercept = 0, col = "gray", linetype = 'dashed') +
+  geom_hline(yintercept = -log10(0.05), col = "gray", linetype = 'dashed') + 
+  geom_point(size = 1) + 
+  scale_color_manual(values = c("#00AFBB", "grey", "#bb0c00"),
+                     labels = c("Downregulated", "Not significant", "Upregulated")) +
+  coord_cartesian(ylim = c(0, 50), xlim = c(-10, 10)) + 
+  labs(color = 'Severe', #legend_title,
+       x = expression("log"[2]*"FoldChange"), y = expression("-log"[10]*"p-value")) + 
+  scale_x_continuous(breaks = seq(-10, 10, 2)) +
+  ggtitle("volcano plot--ddsTxi result") +
+  geom_text() 
+ggsave(paste0(output_path, "/result_vol_plot.png", collapse = NULL), plot = p, width = 8, height = 6, units = "in")
--- scripts/scRNAseq_Easy.py
+++ scripts/scRNAseq_Easy.py
@@ -35,6 +35,10 @@ def main():
     parser.add_argument('group2_zip', type=str, nargs= "?", help='Path to the second zip file.')
     parser.add_argument('-o', '--output', type=str, help='Path to save the output graph.')
     parser.add_argument('-v', '--visual', action='store_true', help='Run visualization of correlation if set to True.')
+    parser.add_argument('-p_value','--p_value', type=float, default=0.05, help='Set p-value threshold for visualization.')
+    parser.add_argument('-fod', '--fod', type=float, default=1, help='Set FOD value for visualization.')
+    parser.add_argument('-filter', '--filter', type=float, default=0, help='filter the genes with count lower than')
+    parser.add_argument('-name', '--name', type=str, default="NO", help='2-column dataset containing the name of the genes')
     
     # Parse the arguments
     args = parser.parse_args()
@@ -49,49 +53,49 @@ def main():
         print_manual()
         return
 
-    # #-------------------------sanity_check (zhqian)---------------------------#
+    #-------------------------sanity_check (zhqian)---------------------------#
 
-    # # check if the first zip file and the second zip file has the same amount of files
-    # zip_file_path_1 = args.group1_zip
-    # zip_file_path_2 = args.group2_zip
-    # zip_file_1_file_count = sanity_check.check_files_amount(zip_file_path_1)
-    # zip_file_2_file_count = sanity_check.check_files_amount(zip_file_path_2)
+    # check if the first zip file and the second zip file has the same amount of files
+    zip_file_path_1 = args.group1_zip
+    zip_file_path_2 = args.group2_zip
+    zip_file_1_file_count = sanity_check.check_files_amount(zip_file_path_1)
+    zip_file_2_file_count = sanity_check.check_files_amount(zip_file_path_2)
 
-    # # if there are not the same amount of files in zip file path, then end the program
-    # if not sanity_check.check_if_same_amount_of_files(zip_file_1_file_count, zip_file_2_file_count):
-    #     return
+    # if there are not the same amount of files in zip file path, then end the program
+    if not sanity_check.check_if_same_amount_of_files(zip_file_1_file_count, zip_file_2_file_count):
+        return
     
-    # # unzip two input zip_file_path and output it in a new directory
-    # new_direct1 = 'datasets_home1'
-    # source_path1 = unzip_files(zip_file_path_1, new_direct1)
+    # unzip two input zip_file_path and output it in a new directory
+    new_direct1 = 'datasets_home1'
+    source_path1 = unzip_files(zip_file_path_1, new_direct1)
 
-    # new_direct2 = 'datasets_home2'
-    # source_path2 = unzip_files(zip_file_path_2, new_direct2)
+    new_direct2 = 'datasets_home2'
+    source_path2 = unzip_files(zip_file_path_2, new_direct2)
 
-    # # to create the list of csv file
-    # csv_files_list_1 = sanity_check.convert_files_to_csv(source_path1, df_dir_name="dataframe_home")
-    # csv_files_list_2 = sanity_check.convert_files_to_csv(source_path2, df_dir_name="dataframe_home")
+    # to create the list of csv file
+    csv_files_list_1 = sanity_check.convert_files_to_csv(source_path1, df_dir_name="dataframe_home")
+    csv_files_list_2 = sanity_check.convert_files_to_csv(source_path2, df_dir_name="dataframe_home")
 
-    # #--------------------------------------------------------------------------#
+    #--------------------------------------------------------------------------#
 
 
-    # #----------------------------------correlation(Sicheng)---------------------------------------#
+    #----------------------------------correlation(Sicheng)---------------------------------------#
     
-    # group_1_df = []
-    # group_2_df = []
-    # correlation(group_1_df, group_2_df)
+    group_1_df = []
+    group_2_df = []
+    correlation(group_1_df, group_2_df)
     
     #---------------------------------------------------------------------------------------------#
 
-    group_1 = ["a","b","c","d","e","f","g","h","i"]
+    group_1 = []
     group_1_R = json.dumps(group_1)
-    group_2 = ["a","b","c","d","e","f","g","h","i"]
+    group_2 = []
     group_2_R = json.dumps(group_2)
 
     #  Pass processed data and output path to the R script
     if args.visual:
         r_script_path = resource_filename(__name__, '../scripts/data_vis.R')
-        dp.run_r_script(group_1_R, group_2_R, args.output, r_script_path)
+        dp.run_r_script(group_1_R, group_2_R, args.output, args.p_value, args.fod, args.filter, args.name, r_script_path)
     else:
         print("Visualization is skipped as -v or --visual flag is not set.")
 
