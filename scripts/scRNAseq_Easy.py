b6195ddb19949defea7c00abaad9af88b962b321
JerryLiu314 <jil314@ucsd.edu>
2024-05-27 22:35:02 -0700

    R test

--- scripts/scRNAseq_Easy.py
+++ scripts/scRNAseq_Easy.py
@@ -6,8 +6,8 @@ import sys
 import json
 from pkg_resources import resource_filename
 import packages.R_prep as dp
-import sanity_check
-import correlation
+import packages.sanity_check as sanity_check
+import packages.correlation as correlation
 
 
 # this method unzip file into a new directory
@@ -48,43 +48,43 @@ def main():
         print_manual()
         return
 
-    #-------------------------sanity_check (zhqian)---------------------------#
+    # #-------------------------sanity_check (zhqian)---------------------------#
 
-    # check if the first zip file and the second zip file has the same amount of files
-    zip_file_path_1 = args.group1_zip
-    zip_file_path_2 = args.group2_zip
-    zip_file_1_file_count = sanity_check.check_files_amount(zip_file_path_1)
-    zip_file_2_file_count = sanity_check.check_files_amount(zip_file_path_2)
+    # # check if the first zip file and the second zip file has the same amount of files
+    # zip_file_path_1 = args.group1_zip
+    # zip_file_path_2 = args.group2_zip
+    # zip_file_1_file_count = sanity_check.check_files_amount(zip_file_path_1)
+    # zip_file_2_file_count = sanity_check.check_files_amount(zip_file_path_2)
 
-    # if there are not the same amount of files in zip file path, then end the program
-    if not sanity_check.check_if_same_amount_of_files(zip_file_1_file_count, zip_file_2_file_count):
-        return
+    # # if there are not the same amount of files in zip file path, then end the program
+    # if not sanity_check.check_if_same_amount_of_files(zip_file_1_file_count, zip_file_2_file_count):
+    #     return
     
-    # unzip two input zip_file_path and output it in a new directory
-    new_direct1 = 'datasets_home1'
-    source_path1 = unzip_files(zip_file_path_1, new_direct1)
+    # # unzip two input zip_file_path and output it in a new directory
+    # new_direct1 = 'datasets_home1'
+    # source_path1 = unzip_files(zip_file_path_1, new_direct1)
 
-    new_direct2 = 'datasets_home2'
-    source_path2 = unzip_files(zip_file_path_2, new_direct2)
+    # new_direct2 = 'datasets_home2'
+    # source_path2 = unzip_files(zip_file_path_2, new_direct2)
 
-    # to create the list of csv file
-    csv_files_list_1 = sanity_check.convert_files_to_csv(source_path1, df_dir_name="dataframe_home")
-    csv_files_list_2 = sanity_check.convert_files_to_csv(source_path2, df_dir_name="dataframe_home")
+    # # to create the list of csv file
+    # csv_files_list_1 = sanity_check.convert_files_to_csv(source_path1, df_dir_name="dataframe_home")
+    # csv_files_list_2 = sanity_check.convert_files_to_csv(source_path2, df_dir_name="dataframe_home")
 
-    #--------------------------------------------------------------------------#
+    # #--------------------------------------------------------------------------#
 
 
-    #----------------------------------correlation(Sicheng)---------------------------------------#
+    # #----------------------------------correlation(Sicheng)---------------------------------------#
     
-    group_1_df = []
-    group_2_df = []
-    correlation(group_1_df, group_2_df)
+    # group_1_df = []
+    # group_2_df = []
+    # correlation(group_1_df, group_2_df)
     
     #---------------------------------------------------------------------------------------------#
 
-    group_1 = []
+    group_1 = ["a","b","c","d","e","f","g","h","i"]
     group_1_R = json.dumps(group_1)
-    group_2 = []
+    group_2 = ["a","b","c","d","e","f","g","h","i"]
     group_2_R = json.dumps(group_2)
 
     #  Pass processed data and output path to the R script
