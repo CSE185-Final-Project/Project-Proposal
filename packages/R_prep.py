3a22094a314854e8c76b7cf26b8c6363e51fd779
JerryLiu314 <jil314@ucsd.edu>
2024-05-29 13:15:48 -0700

    correlation bug fixing

--- packages/R_prep.py
+++ packages/R_prep.py
@@ -3,6 +3,6 @@ import pandas as pd
 import subprocess
 from pkg_resources import resource_filename
 
-def run_r_script(data1, data2, output_path, R_path):
+def run_r_script(group_1_R, group_2_R, output, p_value, fod, filter, name, deseq2, R_path):
     # Call the R script
-    subprocess.run(["Rscript", R_path, data1, data2, output_path])
+    subprocess.run(["Rscript", R_path, group_1_R, group_2_R, output, p_value, fod, filter, name, deseq2])
