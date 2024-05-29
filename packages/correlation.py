462ebea45d2f68828cd17432f78c88813da0a28a
sichengjing <114368537+sichengjing@users.noreply.github.com>
2024-05-28 19:51:11 -0700

    Update correlation.py

--- packages/correlation.py
+++ packages/correlation.py
@@ -20,26 +20,41 @@ def correlation(group1_name, group2_name):
     for i in range(len(group2_name)): tpm_group2[i] = group2_name[i]["TPM"].tolist()
 
     # Adjust TPM numbers
-    for lt in tpm_group1: lt = list(map(lambda x: math.log10(x+1), lt))
-    for lt in tpm_group2: lt = list(map(lambda x: math.log10(x+1), lt))
+    for i in range(len(tpm_group1)):
+        tpm_group1[i] = [math.log10(float(x) + 1) for x in tpm_group1[i]]
+    for i in range(len(tpm_group2)):
+        tpm_group2[i] = [math.log10(float(x) + 1) for x in tpm_group2[i]]
 
     # Generate correlations
-    for i in range(0, len(tpm_group1) - 1):
+    for i in range(0, len(tpm_group1)-1):
         for j in range(i+1, len(tpm_group1)):
-            r = 0.0
-            r = stats.pearsonr(tpm_group1[i], tpm_group1[j])[0]
-            print("----------- First Group Pearson Correlation Results -----------")
-            print("Group 1 input%d vs. Group 1 input%d: %.4f" % (i, j, r))
-            if r < 0.3: # Adjustment may be needed.
-                print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+            if len(tpm_group1[i]) == len(tpm_group1[j]):
+                res = stats.pearsonr(tpm_group1[i], tpm_group1[j], alternative= "two-sided", method = None)
+                print("----------- First Group Pearson Correlation Results -----------")
+                print("Sample size of Group 1 input %d: %d" % (i+1, len(tpm_group1[i])))
+                print("Sample size of Group 1 input %d: %d" % (j+1, len(tpm_group1[j])))
+                print("Correlation of Group 1 input %d vs. Group 1 input %d: %.4f" % (i+1, j+1, res[0]))
+                if res[0] < 0.3: # Adjustment may be needed.
+                    print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+                print("P-value: %.4f " % (res[1]))
+                print("Significant level: P < %.4f" % (stats.norm.sf(abs(res[0]))))
+                print("95%", res.confidence_interval(confidence_level=0.95), "for correlation")
+            else:
+                print("WARNING: Group 1 input %d file and Group 1 input %d file don't have the same sample size, so cannot calculate Pearson correlation." % (i+1, j+1))
 
     for i in range(0, len(tpm_group2) - 1):
         for j in range(i+1, len(tpm_group2)):
-            r = 0.0
-            r = stats.pearsonr(tpm_group1[i], tpm_group1[j])[0]
-            print("----------- Second Group Pearson Correlation Results -----------")
-            print("Group 2 input%d vs. Group 2 input%d: %.4f" % (i, j, r))
-            if r < 0.3: # Adjustment may be needed.
-                print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+            if len(tpm_group2[i]) == len(tpm_group2[j]):
+                res = stats.pearsonr(tpm_group2[i], tpm_group2[j])
+                print("----------- Second Group Pearson Correlation Results -----------")
+                print("Sample size of Group 2 input %d: %d" % (i+1, len(tpm_group2[i])))
+                print("Sample size of Group 2 input %d: %d" % (j+1, len(tpm_group2[j])))
+                print("Group 2 input %d vs. Group 2 input %d: %.4f" % (i+1, j+1, res[0]))
+                if res[0] < 0.3: # Adjustment may be needed.
+                    print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+                print("P-value: %.4f " % (res[1]))
+                print("Significant level: P < %.4f" % (stats.norm.sf(abs(res[0]))))
+                print("95%", res.confidence_interval(confidence_level=0.95), "for correlation")
+            else:
+                print("WARNING: Group 2 input %d file and Group 2 input %d file don't have the same sample size, so cannot calculate Pearson correlation." % (i+1, j+1))
 
-    # Generate correlations plots
