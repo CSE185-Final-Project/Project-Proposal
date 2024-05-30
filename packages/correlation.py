4ca5e31f6b96b42bd7a3952c9e3caa375ee9e506
sichengjing <114368537+sichengjing@users.noreply.github.com>
2024-05-29 17:37:20 -0700

    Update correlation.py

--- packages/correlation.py
+++ packages/correlation.py
@@ -27,38 +27,57 @@ def correlation(group1_name, group2_name):
 
     # Generate correlations
     print("----------- First Group Pearson Correlation Results -----------")
+    print("Sample size of Group 1 samples: %d" % (len(tpm_group1[0])))
+    print("")
+    countpass = 0
+    count = 0
     for i in range(0, len(tpm_group1)-1):
         for j in range(i+1, len(tpm_group1)):
-            if len(tpm_group1[i]) == len(tpm_group1[j]):
-                res = stats.pearsonr(tpm_group1[i], tpm_group1[j], alternative= "two-sided", method = None)
-                print("Sample size of Group 1 input %d: %d" % (i+1, len(tpm_group1[i])))
-                print("Sample size of Group 1 input %d: %d" % (j+1, len(tpm_group1[j])))
-                print("Correlation of Group 1 input %d vs. Group 1 input %d: %.4f" % (i+1, j+1, res[0]))
-                if res[0] < 0.3: # Adjustment may be needed.
-                    print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
-                print("P-value: %.4f " % (res[1]))
-                print("Significant level: P < %.4f" % (stats.norm.sf(abs(res[0]))))
-                print("95%", res.confidence_interval(confidence_level=0.95), "for correlation")
-                print("")
+            count += 1
+            res = stats.pearsonr(tpm_group1[i], tpm_group1[j], alternative= "two-sided", method = None)
+            print("Correlation of Group 1 sample %d vs. Group 1 sample %d: %.4f" % (i+1, j+1, res[0]))
+            if res[0] < 0.3: # Adjustment may be needed.
+                print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+            if res[1] > 0.05:
+                print("WARNING: The Pearson correlation is not statistically significant since it greater than 0.05.")
+                print("Your Pearson correlation: %.4f" %(res[1]))
+            if res[0] >= 0.3 and res[1] <= 0.05:
+                countpass += 1
+            if res[1] < 0.0001:
+                print("Significant level: P < 0.0001")
             else:
-                print("WARNING: Group 1 input %d file and Group 1 input %d file don't have the same sample size, so cannot calculate Pearson correlation." % (i+1, j+1))
+                print("Significant level: P < %.4f" % (res[1]))
+            print("95%", res.confidence_interval(confidence_level=0.95), "for this correlation coefficient")
+            print("")
+    if count == countpass:
+        print("Group 1 passed the correlation checks. Samples in this group are associated with each other.")
+
 
     print("")
     print("----------- Second Group Pearson Correlation Results -----------")
+    print("Sample size of Group 2 samples: %d" % (len(tpm_group2[0])))
+    print("")
+    countpass = 0
+    count = 0
     for i in range(0, len(tpm_group2) - 1):
         for j in range(i+1, len(tpm_group2)):
-            if len(tpm_group2[i]) == len(tpm_group2[j]):
-                res = stats.pearsonr(tpm_group2[i], tpm_group2[j], alternative= "two-sided", method = None)
-                print("Sample size of Group 2 input %d: %d" % (i+1, len(tpm_group2[i])))
-                print("Sample size of Group 2 input %d: %d" % (j+1, len(tpm_group2[j])))
-                print("Correlation of Group 2 input %d vs. Group 2 input %d: %.4f" % (i+1, j+1, res[0]))
-                if res[0] < 0.3: # Adjustment may be needed.
-                    print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
-                print("P-value: %.4f " % (res[1]))
-                print("Significant level: P < %.4f" % (stats.norm.sf(abs(res[0]))))
-                print("95%", res.confidence_interval(confidence_level=0.95), "for correlation")
-                print("")
+            count += 1
+            res = stats.pearsonr(tpm_group2[i], tpm_group2[j], alternative= "two-sided", method = None)
+            print("Correlation of Group 2 sample %d vs. Group 2 sample %d: %.4f" % (i+1, j+1, res[0]))
+            if res[0] < 0.3: # Adjustment may be needed.
+                print("WARNING: The Pearson correlation result is below 0.3. You may want to check the quality of the input files.")
+            if res[1] > 0.05:
+                print("WARNING: The Pearson correlation is not statistically significant since it greater than 0.05.")
+                print("Your Pearson correlation: %.4f" % (res[1]))
+            if res[0] >= 0.3 and res[1] <= 0.05:
+                countpass += 1
+            if res[1] < 0.0001:
+                print("Significant level: P < 0.0001")
             else:
-                print("WARNING: Group 2 input %d file and Group 2 input %d file don't have the same sample size, so cannot calculate Pearson correlation." % (i+1, j+1))
+                print("Significant level: P < %.4f" % (res[1]))
+            print("95%", res.confidence_interval(confidence_level=0.95), "for this correlation coefficient")
+            print("")
+    if count == countpass:
+        print("Group 2 passed the correlation checks. Samples in this group are associated with each other.")
 
 
